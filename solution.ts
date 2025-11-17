13ae7383ab1fad9d0dcded2f6cf2229fd3bc3a4d
Salauddin-Ahmad <salauddinahmed555@gmail.com>
2025-11-17 12:24:52 +0600

    FIX: added the function return type problem1

--- solution.ts
+++ solution.ts
@@ -1,26 +1,14 @@
-//  Problem 1:
+type ValueType = string | number | boolean;
 
-type ValueType =  string | number | boolean;
-
-const formatValue = (value: ValueType) => {
+const formatValue = (value: ValueType): ValueType => {
   if (typeof value === "string") {
-    const valueUppercase = value.toUpperCase();
-    return valueUppercase;
+    return value.toUpperCase();
   }
 
   if (typeof value === "number") {
     return value * 10;
   }
 
-  if (typeof value === "boolean") {
-    if (value === true) {
-      return false;
-    } else if (value === false) {
-      return true;
-    }
-  }
+  return !value;
 };
 
-console.log(formatValue("hello"));
-// console.log(formatValue(5));
-// console.log(formatValue(true));
